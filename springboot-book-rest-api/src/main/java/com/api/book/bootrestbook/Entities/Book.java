61b9f30e5e732edbc625050214e51324219f15ed
pathanNajaf1 <pathannajaf74@gmail.com>
2026-03-24 21:54:06 +0530

    Update Book.java

--- springboot-book-rest-api/src/main/java/com/api/book/bootrestbook/Entities/Book.java
+++ springboot-book-rest-api/src/main/java/com/api/book/bootrestbook/Entities/Book.java
@@ -1,37 +1,58 @@
 package com.api.book.bootrestbook.Entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
+@Entity
+@Table(name="books")
 public class Book {
-    private int  id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    private String title;
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(Integer id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Integer getId() {   
+        return id;
+    }
+
+    public void setId(Integer id) {  
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     @Override
     public String toString() {
         return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
     }
-    private String title;
-    private String author;
-    public Book() {
-    }
-    public Book(int id, String title, String author) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-                    
 }
